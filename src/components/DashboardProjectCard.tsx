c27b3aafd352d94cc735532a6e4d0a8386f77e2e
Henrique <hp_bonfim@hotmail.com>
2020-10-05 18:29:17 -0300

    finish the Dockerfile.prod and Add docker-compose for test the application image for production

--- src/components/DashboardProjectCard.tsx
+++ src/components/DashboardProjectCard.tsx
@@ -4,7 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import NewProjectPopup from './NewProjectPopup';
+import NewProjectPopup from './newProjectPopup';
 import NewTaskBoardPopup from './NewTaskBoardPopup';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
